3fe42105d9989deef4c86805155003e84ca14ed4
Emily Holt <emily.holt@dartmouth.edu>
2020-02-26 18:25:59 -0500

    Initial commit

---
+++ run.py
@@ -0,0 +1,8 @@
+import experiment
+
+crossover_rate=0.5
+mutation_rate=0.2
+population_size=300
+total_generations=100
+
+experiment.run(crossover_rate, mutation_rate, population_size, total_generations)
